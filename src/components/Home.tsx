c9051a0fd094604282f73088c7868695c60c4fdc
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-07-12 16:02:53 -0700

    fix bug

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import Header from './Header'
 import MinHeader from './MinHeader'
 import AddCoffee from './AddCoffee'
 import BuyCoffeeList from './BuyCoffeeList'
 import Footer from './Footer'
-// type Props = {}
+
+
 
 const Home = () => {
   return (
