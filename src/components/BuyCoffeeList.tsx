4921630f24b627d8dea8ae9a41cbdfa8d28a5b99
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-07-13 06:20:40 -0700

    FIx Bug

--- src/components/BuyCoffeeList.tsx
+++ src/components/BuyCoffeeList.tsx
@@ -53,8 +53,8 @@ const BuyCoffeeList = () => {
       {success && <OnSuccessAlert message={success} />}
       {loading && <LoadingAlert message={loading} />}
       {/* display buyMeCoffee */}
-      <div className=' flex flex-wrap gap-6 mb-3'>
-          <div className='flex-shrink-0 flex-grow-0 w-250 flex max-sm:flex-col'>
+      <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+          <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8'>
             {getCoffee()}
           </div>
       </div>
