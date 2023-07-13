527951019f3fcdef03709e98f74ffacd36f7a94a
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-07-13 06:08:33 -0700

    fix bugs in useContractToCall

--- src/hooks/useContractRead.ts
+++ src/hooks/useContractRead.ts
@@ -1,7 +1,7 @@
 import { useContractRead } from 'wagmi'
 import BuyCoffee from '../abis/buycoffee.json'
 
-export const useContractToCall = (functionName: string, args?: Array<any>, watch?: boolean, from?:`0x${string}` | undefined) => {
+export const useContractToCall = (functionName: string, args?: Array<any>, watch?: boolean) => {
     const resp = useContractRead({
         address: BuyCoffee.address as `0x${string}`,
         abi: BuyCoffee.abi,
