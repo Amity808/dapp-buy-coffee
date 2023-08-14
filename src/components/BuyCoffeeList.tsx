ac6aca0866b0d569d4dede68d366c5d5600feb31
Muhdsodiq Bolarinwa <105107754+Amity808@users.noreply.github.com>
2023-08-13 23:06:35 -0700

    Update BuyCoffeeList.tsx

--- src/components/BuyCoffeeList.tsx
+++ src/components/BuyCoffeeList.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import { useContractToCall } from '../hooks/useContractRead'
 import Coffee from './Coffee'
-import ErrorAlert from './alerts/ErrorAlert'
-import OnSuccessAlert from './alerts/OnSuccessAlert'
-import LoadingAlert from './alerts/LoadingAlert'
+// import ErrorAlert from './alerts/ErrorAlert'
+// import OnSuccessAlert from './alerts/OnSuccessAlert'
+// import LoadingAlert from './alerts/LoadingAlert'
 
 
 const BuyCoffeeList = () => {
@@ -37,9 +37,9 @@ const BuyCoffeeList = () => {
         <Coffee 
         key={index}
         id={index}
-        setSuccess={setSuccess}
+{/*         setSuccess={setSuccess}
         seError={setError}
-        setLoading={setLoading}
+        setLoading={setLoading} */}
         clear={clear}
         />
       )
@@ -49,9 +49,9 @@ const BuyCoffeeList = () => {
   }
   return (
     <div className='mb-3'>
-      {error && <ErrorAlert message={error} clear={clear} />}
+{/*       {error && <ErrorAlert message={error} clear={clear} />}
       {success && <OnSuccessAlert message={success} />}
-      {loading && <LoadingAlert message={loading} />}
+      {loading && <LoadingAlert message={loading} />} */}
       {/* display buyMeCoffee */}
       <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
           <div className='grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8'>
